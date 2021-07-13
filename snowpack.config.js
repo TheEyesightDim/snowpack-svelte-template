2d2fb86ac515f8192c648d2d2190b869cf153348
TheEyesightDim <anisohedron@gmail.com>
2021-07-13 09:37:08 -0400

    improved directory structure

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,12 @@
 // Snowpack Configuration File
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
+//The following comment helps Typescript's type system
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    public: "/public-build",
+    src: "/dist",
   },
   plugins: ["@snowpack/plugin-svelte"],
   packageOptions: {
